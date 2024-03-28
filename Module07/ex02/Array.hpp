44566925d721450073b60cc59450ec7d394f34d5
zemblabla7 <cbsomarriba@gmail.com>
2024-03-28 15:40:22 +0100

    module07 ex02 verifs en cours

--- Module07/ex02/Array.hpp
+++ Module07/ex02/Array.hpp
@@ -15,69 +15,44 @@ class Array
 		Array() {};
 		~Array()
 		{
-			// for (int i = 0; i < size(); i++) //boucle necessaire?
-			// 	delete _array[i];
-			//delete _array;
-			//delete[] _array;
+			delete[] _array; //IMPORTANT delete[] for arrays (int *array = new int[]), delete for single objects (int *unit = new int)
 		};
 
 		Array(Array const &copy)
 		{
-			// for (int i = 0; i < copy._array.size(); i++)
+			_size = copy._size;
+			_array = new T[_size];
 			for (unsigned int i = 0; i < _size; i++)
 				_array[i] = copy._array[i];
 		};
 
 		Array &operator=(Array const &rhs)
 		{
-			delete[] _array; //avec []
-			// _array = new T[rhs._array.size()];
+			delete[] _array;
+			_size = rhs._size;
 			_array = new T[_size];
-			// for (int i = 0; i < rhs._array.size(); i++)
 			for (unsigned int i = 0; i < _size; i++)
 				_array[i] = rhs._array[i];
+			return *this;
 		};
 
-		Array(unsigned int n) : _size(n)
+		Array(unsigned int n)
 		{
+			_size = n;
 			_array = new T[n];
-			// std::fill_n(_array, n, T()); //to fill with default value (0 for digits for example)
+			std::fill_n(_array, n, T()); //to fill with default value (0 for digits for example)
+			/*IMPORTANT; T() instead of T allows to directly initialize a type. Ex:
+			int() = 0, double() = 0.0, std, string() = "" */
 		};
 		unsigned int size() const
 		{
-			//return sizeof(_array)/sizeof(_array[0]); //sizeof() instead of size() because it's an array
+			//IMPORTANT return sizeof(_array)/sizeof(_array[0]); //sizeof() instead of size() because it's an array
 			return _size;
 		};
-/* 		T &operator[](size_t index) //operator [] pour les objets de type T
-		{
-			try
-			{
-				if (_array[index] != NULL)
-					return _array[index];
-			}
-			catch(const std::exception& e) {};
-		} */
-		/* T &operator[](size_t index) //operator [] pour les objets de type T
-		{
-			try
-			{
-				if (index >= size())
-				{
-					std::cout<<"Index out of array range" <<std::endl;
-					throw std::exception();
-				}
-				//return _array[index];
-			}
-			catch(const std::exception& e) {};
-			return _array[index];
-		}; */
 		T &operator[](size_t index) //operator [] pour les objets de type T
 		{
 			if (index >= size())
-			{
-				std::cout<<"Index out of array range" <<std::endl;
-				throw std::exception();
-			}
+				throw std::out_of_range("Index out of array range");
 			return _array[index];
 		};
 };
--- Module07/ex02/main.cpp
+++ Module07/ex02/main.cpp
@@ -54,13 +54,69 @@ int main(int, char**)
 
 int main()
 {
-	Array<int> array(4);
-	for (unsigned int i = 0; i < array.size(); i++)
-		array[i] = i;
-	std::cout <<"Array size = " <<array.size() <<std::endl;
-	for (unsigned int i = 0; i < array.size(); i++)
-		std::cout <<"array[" <<i <<"] : " <<array[i] <<std::endl;
+	try
+	{
+	//CHECK CLASS WITH INT AND CHAR
+		Array<int> array(4);
+		for (unsigned int i = 0; i < array.size(); i++)
+			array[i] = i;
+		std::cout <<"Array size = " <<array.size() <<std::endl;
+		for (unsigned int i = 0; i < array.size(); i++)
+			std::cout <<"array[" <<i <<"] : " <<array[i] <<std::endl;
+		std::cout <<std::endl;
+
+		Array<std::string> strArray(4);
+		for (unsigned int i = 0; i < strArray.size(); i++)
+			strArray[i] = 'a' + i;
+		std::cout <<"StrstrArray size = " <<strArray.size() <<std::endl;
+		for (unsigned int i = 0; i < strArray.size(); i++)
+			std::cout <<"strArray[" <<i <<"] : " <<strArray[i] <<std::endl;
+		std::cout <<std::endl;
+
+	//CHECK OPERATOR = DEEP COPY
+		Array<int> array1 = array;
+		std::cout <<"Array1 size = " <<array1.size() <<std::endl;
+		for (unsigned int i = 0; i < array1.size(); i++)
+			std::cout <<"array1[" <<i <<"] : " <<array1[i] <<std::endl;
+		std::cout <<std::endl;
+
+	//CHECK CONSTRUCTOR DEEP COPY
+		Array<int> array2(array1);
+		std::cout <<"Array2 size = " <<array2.size() <<std::endl;
+		for (unsigned int i = 0; i < array2.size(); i++)
+			std::cout <<"array2[" <<i <<"] : " <<array2[i] <<std::endl;
+		std::cout <<std::endl;
 	
-	Array<int> copy_array = array;
+	//CHECK INDEX OUT OF RANGE
+		array2[7] = 4;
+	}
+	catch(const std::exception& e) {}
+
+	try
+	{
+		//Creates an array of n elements initialized by default.
+		int * a = new int();
+		std::cout <<"TEST : " <<a <<std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	try
+	{
+		//Creates an empty array.
+		Array<int> array;
+		for (unsigned int i = 0; i < array.size(); i++)
+			std::cout <<"array[" <<i <<"] : " <<array[i] <<std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	
+	
+
 	return 0;
 }
