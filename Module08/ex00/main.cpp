3d9747cd0545f861821a6589738e6aeae76e182e
zemblabla7 <cbsomarriba@gmail.com>
2024-03-28 19:19:28 +0100

    module08 et 09 subjects

---
+++ Module08/ex00/main.cpp
@@ -0,0 +1,4 @@
+//lire Consignes générales du sujet (changent pour CPP8 et 9)
+//video intra: https://elearning.intra.42.fr/notions/piscine-c-d08-stl/subnotions/piscine-c-d08-stl-stl/videos/stl-1e60e486-1b8b-4f10-89bd-b2b3871bd3e9
+
+include <algorithm> --> le but est d utiliser des containers de la STL
